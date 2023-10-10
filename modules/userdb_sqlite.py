5af5656adc6bd52109db9f80a0b186dc984525f1
yassineosip <yassine.lf.99@gmail.com>
2023-10-10 16:15:00 +0100

    adding user infos CRUD

--- modules/userdb_sqlite.py
+++ modules/userdb_sqlite.py
@@ -7,8 +7,8 @@ import json
 logging.basicConfig(filename="errors.log", filemode="w", level=logging.DEBUG)
 
 class CustomUser():
-    def __init__(self, login, password, infos={}) -> None:
-        self.login = login
+    def __init__(self, username, password, infos={}):
+        self.username = username
         self.password = password
         self.infos = infos
 
@@ -23,44 +23,79 @@ class CustomUser():
     
 class DB():
     def __init__(self):
-        conn = sqlite3.connect("HacksimDb.db")
+        conn = sqlite3.connect("hacksim.db")
         self.conn = conn
-        conn.execute('''CREATE TABLE IF NOT EXISTS USER
-                                            (LOGIN TEXT NOT NULL,
-                                            PASSWORD TEXT NOT NULL,
-                                            INFOS TEXT NOT NULL);
+        conn.execute('''CREATE TABLE IF NOT EXISTS users
+                                            (username TEXT NOT NULL,
+                                            password TEXT NOT NULL,
+                                            infos TEXT NOT NULL);
                      ''')
         conn.commit()
 
-    def hashPassword(self, password):
+    def hash_password(self, password):
         salt = uuid.uuid4().hex
         return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ":" + salt
 
-    def selectByLogin(self, login):
+    def select_by_login(self, username):
         try:
-            cursor = self.conn.execute("SELECT LOGIN, PASSWORD FROM USER WHERE LOGIN = '%s'" % login)
+            cursor = self.conn.execute("SELECT username, password FROM users WHERE username = '%s'" % username)
             user = cursor.fetchone()
             if user == None:
                 return False
             return user
         except Exception as ex:
-            logging.error("selectionByLogin failed,got this message : %s" % ex)
+            logging.error("selectionByLogin failed, got this message : %s" % ex)
 
     def register(self, user):
         try:
-            if self.selectByLogin(user.login) == False:
-                self.conn.execute("INSERT INTO USER (LOGIN, PASSWORD, INFOS)\
-                                VALUES ('%s', '%s', '%s')" % (user.login, self.hashPassword(user.password), json.dumps(user.infos)))
+            if self.select_by_login(user.username) == False:
+                self.conn.execute("INSERT INTO users (username, password, infos)\
+                                VALUES ('%s', '%s', '%s')" % (user.username, self.hash_password(user.password), json.dumps(user.infos)))
                 self.conn.commit()
                 return True
-            else:
-                return False
+            return False
         except Exception as ex:
-            logging.error("registration failed,got this message : %s" % ex)
+            logging.error("registration failed, got this message : %s" % ex)
 
     def login(self, login, password):
-        user = self.selectByLogin(login)
+        user = self.select_by_login(login)
         if user == False:
             return False
         hpassword, salt = user[1].split(":")
         return hpassword == hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+    
+    def add_or_update_user_infos(self, username, infos):
+        try:
+            cursor = self.conn.execute("SELECT infos FROM users WHERE username = '%s'" % username)
+            user = cursor.fetchone()
+            if user == None:
+                return False
+            user_infos = json.loads(user[0])
+            user_infos.update(infos)
+            self.conn.execute("UPDATE users SET infos = '%s' WHERE username = '%s'" % (json.dumps(user_infos), username))
+            self.conn.commit()
+            return True
+        except Exception as ex:
+            logging.error("add_or_update_user_infos failed, got this message : %s" % ex)
+
+    def delete_user_infos_by_key(self, username, key):
+        try:
+            infos = self.get_user_infos(username)
+            if infos == False:
+                return False
+            infos.pop(key)
+            self.conn.execute("UPDATE users SET infos = '%s' WHERE username = '%s'" % (json.dumps(infos), username))
+            self.conn.commit()
+            return True
+        except Exception as ex:
+            logging.error("delete_user_infos_by_key failed, got this message : %s" % ex)
+
+    def get_user_infos(self, username):
+        try:
+            cursor = self.conn.execute("SELECT infos FROM users WHERE username = '%s'" % username)
+            user = cursor.fetchone()
+            if user == None:
+                return False
+            return json.loads(user[0])
+        except Exception as ex:
+            logging.error("get_user_infos failed, got this message : %s" % ex)
--- modules/userdb_sqllite_tests.py
+++ modules/userdb_sqllite_tests.py
@@ -2,17 +2,31 @@ import unittest
 from userdb_sqlite import CustomUser, DB
 
 class SimpleTest(unittest.TestCase):
-    def test(self):
+    db = DB()
+
+    def test1(self):
+        # user with infos
         user1 = CustomUser("test1", "test1", {"test1": "test1"})
-        user2 = CustomUser("test2", "test2", {"test2": "test2"})
-        db = DB()
-
+        # user without infos
+        user2 = CustomUser("test2", "test2")
         #register
-        self.assertTrue(db.register(user1))
-        self.assertTrue(db.register(user2))
+        self.assertTrue(self.db.register(user1))
+        self.assertTrue(self.db.register(user2))
         #login
-        self.assertTrue(db.login("test1", "test1"))
-        self.assertTrue(db.login("test2", "test2"))
+        self.assertTrue(self.db.login("test1", "test1"))
+        self.assertTrue(self.db.login("test2", "test2"))
 
+    def test2(self):
+        #add infos
+        self.assertTrue(self.db.add_or_update_user_infos("test1", {"test2": "test2"}))
+        self.assertTrue(self.db.add_or_update_user_infos("test2", {"test1": "test1"}))
+        #get infos
+        self.assertEqual(self.db.get_user_infos("test1"), {"test1": "test1", "test2": "test2"})
+        self.assertEqual(self.db.get_user_infos("test2"), {"test1": "test1"})
+        #update infos
+        self.assertTrue(self.db.add_or_update_user_infos("test2", {"test11": "test11", "delete": "delete"}))
+        #delete infos by key
+        self.assertTrue(self.db.delete_user_infos_by_key("test2", "delete"))
+        
 if __name__ == '__main__':
     unittest.main()
